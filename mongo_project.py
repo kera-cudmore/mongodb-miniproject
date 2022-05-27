3f8067dec55d8711cb3a52d4713c5f382eb45743
Kera Cudmore <kera.cudmore@hotmail.com>
2022-05-27 12:05:16 +0000

    add: new_doc function to create a record in DB. get_doc function for searching DB

--- mongo_project.py
+++ mongo_project.py
@@ -45,7 +45,36 @@ def show_menu():
     return option
 
 
+def get_record():
+    """
+    search function - use names for finding data
+    """
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    # searches the database for first & last name (converted to lowercase)
+    try:
+        doc = coll.find_one({"first": first.lower(), "last": last.lower()})
+
+    # message shown if error accessing the database
+    except:
+        print("Error accessing the database")
+
+    # if no document is returned, record looking for not found
+    # an empty cursor object is returned
+    if not doc:
+        print("")
+        print("Error! No results found.")
+    
+    return doc
+
+
 def add_record():
+    """
+    add record function
+    Prompts the user to enter the following information
+    """
     print("")
     first = input("Enter first name > ")
     last = input("Enter last name > ")
@@ -55,7 +84,35 @@ def add_record():
     occupation = input("Enter occupation > ")
     nationality = input("Enter nationality > ")
 
+
+# new_doc variable - dictionary with info entered by user
+# use .lower() on first & last names to store them in lower case
+# (makes it easier to find items later)
+    new_doc = {
+        "first": first.lower(),
+        "last": last.lower(),
+        "dob": dob,
+        "gender": gender,
+        "hair_color": hair_color,
+        "occupation": occupation,
+        "nationality": nationality
+    }
+
+    # insert the new_doc variable to the DB
+    try:
+        coll.insert_one(new_doc)
+        print("")
+        print("Document inserted")
+
+    # if data can't be inserted show error message
+    except:
+        print("Error accessing the database")
+
+
 def main_loop():
+    """
+    main loop for options
+    """
     while True:
         option = show_menu()
         if option == "1":
